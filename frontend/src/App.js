469438a45d4d0f0050d6c6f1906b051e18e15b0c
fojtas <fojtik.jan98@gmail.com>
2020-04-29 01:32:26 +0200

    AdminLogin/apoloClient

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-import Header from './Header.js';
-import Content from './Content.js';
+import Header from "./Header.js";
+import Content from "./Content.js";
 
-import './style.css';
+import "./style.css";
 
+const client = new ApolloClient({
+  uri: "http://localhost:5005/graphql",
+});
 
 const App = () => {
+  const [admin, setAdmin] = useState(false);
 
-    const [admin, setAdmin] = useState(false);
-
-    return (
-        <div>
-            <Header setAdmin={setAdmin} />
-            <Content admin={admin} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <Header setAdmin={setAdmin} />
+        <Content admin={admin} />
+      </ApolloProvider>
+    </div>
+  );
+};
 
 export default App;
--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -1,76 +1,142 @@
-import React from 'react';
+import React from "react";
+import AdminZone from "./AdminZone";
 
+const Content = (props) => {
+  return (
+    <div className="content">
+      {props.admin ? (
+        <AdminZone />
+      ) : (
+        <div>
+          <div className="chapterHeader">
+            <h1 id="work">Ukázky realizací</h1>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam
+              neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget
+              erat. Aenean fermentum risus id tortor. Fusce consectetuer risus a
+              nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus quis
+              justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy
+              ac, erat. Nam sed tellus id magna elementum tincidunt. Aliquam
+              ante. Nulla pulvinar eleifend sem. Nulla turpis magna, cursus sit
+              amet, suscipit a, interdum id, felis. Duis viverra diam non justo.
+              Praesent vitae arcu tempor neque lacinia pretium. Vivamus ac leo
+              pretium faucibus.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam
+              neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget
+              erat. Aenean fermentum risus id tortor. Fusce consectetuer risus a
+              nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus quis
+              justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy
+              ac, erat. Nam sed tellus id magna elementum tincidunt. Aliquam
+              ante. Nulla pulvinar eleifend sem. Nulla turpis magna, cursus sit
+              amet, suscipit a, interdum id, felis. Duis viverra diam non justo.
+              Praesent vitae arcu tempor neque lacinia pretium. Vivamus ac leo
+              pretium faucibus.
+            </p>
+          </div>
 
-const Content = props => {
-    return (
+          <div>
+            <div className="chapterHeader">
+              <h1 id="prices">Ceník</h1>
+            </div>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam
+                neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget
+                erat. Aenean fermentum risus id tortor. Fusce consectetuer risus
+                a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus
+                quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non,
+                nonummy ac, erat. Nam sed tellus id magna elementum tincidunt.
+                Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna,
+                cursus sit amet, suscipit a, interdum id, felis. Duis viverra
+                diam non justo. Praesent vitae arcu tempor neque lacinia
+                pretium. Vivamus ac leo pretium faucibus.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam
+                neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget
+                erat. Aenean fermentum risus id tortor. Fusce consectetuer risus
+                a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus
+                quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non,
+                nonummy ac, erat. Nam sed tellus id magna elementum tincidunt.
+                Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna,
+                cursus sit amet, suscipit a, interdum id, felis. Duis viverra
+                diam non justo. Praesent vitae arcu tempor neque lacinia
+                pretium. Vivamus ac leo pretium faucibus.
+              </p>
+            </div>
+          </div>
 
-        <div className="content">
-            {
-                props.admin ?
-                    <div>
-                        <input type="file" />
-                    </div>
-                    :
-                    <div>
-                        <div className="chapterHeader">
-                            <h1 id="work">Ukázky realizací</h1>
-                        </div>
-                        <div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget erat. Aenean fermentum risus id tortor. Fusce consectetuer risus a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nam sed tellus id magna elementum tincidunt. Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Duis viverra diam non justo. Praesent vitae arcu tempor neque lacinia pretium. Vivamus ac leo pretium faucibus.
-                            </p>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget erat. Aenean fermentum risus id tortor. Fusce consectetuer risus a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nam sed tellus id magna elementum tincidunt. Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Duis viverra diam non justo. Praesent vitae arcu tempor neque lacinia pretium. Vivamus ac leo pretium faucibus.
-                            </p>
-                        </div>
+          <div>
+            <div className="chapterHeader">
+              <h1 id="order">Kontaktní formulář pro kalkulaci</h1>
+            </div>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam
+                neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget
+                erat. Aenean fermentum risus id tortor. Fusce consectetuer risus
+                a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus
+                quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non,
+                nonummy ac, erat. Nam sed tellus id magna elementum tincidunt.
+                Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna,
+                cursus sit amet, suscipit a, interdum id, felis. Duis viverra
+                diam non justo. Praesent vitae arcu tempor neque lacinia
+                pretium. Vivamus ac leo pretium faucibus.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam
+                neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget
+                erat. Aenean fermentum risus id tortor. Fusce consectetuer risus
+                a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus
+                quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non,
+                nonummy ac, erat. Nam sed tellus id magna elementum tincidunt.
+                Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna,
+                cursus sit amet, suscipit a, interdum id, felis. Duis viverra
+                diam non justo. Praesent vitae arcu tempor neque lacinia
+                pretium. Vivamus ac leo pretium faucibus.
+              </p>
+            </div>
+          </div>
 
-                        <div>
-                            <div className="chapterHeader">
-                                <h1 id="prices">Ceník</h1>
-                            </div>
-                            <div>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget erat. Aenean fermentum risus id tortor. Fusce consectetuer risus a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nam sed tellus id magna elementum tincidunt. Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Duis viverra diam non justo. Praesent vitae arcu tempor neque lacinia pretium. Vivamus ac leo pretium faucibus.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget erat. Aenean fermentum risus id tortor. Fusce consectetuer risus a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nam sed tellus id magna elementum tincidunt. Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Duis viverra diam non justo. Praesent vitae arcu tempor neque lacinia pretium. Vivamus ac leo pretium faucibus.
-                                </p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="chapterHeader">
-                                <h1 id="order">Kontaktní formulář pro kalkulaci</h1>
-                            </div>
-                            <div>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget erat. Aenean fermentum risus id tortor. Fusce consectetuer risus a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nam sed tellus id magna elementum tincidunt. Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Duis viverra diam non justo. Praesent vitae arcu tempor neque lacinia pretium. Vivamus ac leo pretium faucibus.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget erat. Aenean fermentum risus id tortor. Fusce consectetuer risus a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nam sed tellus id magna elementum tincidunt. Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Duis viverra diam non justo. Praesent vitae arcu tempor neque lacinia pretium. Vivamus ac leo pretium faucibus.
-                                </p>
-
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="chapterHeader">
-                                <h1 id="contacts">Kontakty</h1>
-                            </div>
-                            <div>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget erat. Aenean fermentum risus id tortor. Fusce consectetuer risus a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nam sed tellus id magna elementum tincidunt. Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Duis viverra diam non justo. Praesent vitae arcu tempor neque lacinia pretium. Vivamus ac leo pretium faucibus.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget erat. Aenean fermentum risus id tortor. Fusce consectetuer risus a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nam sed tellus id magna elementum tincidunt. Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Duis viverra diam non justo. Praesent vitae arcu tempor neque lacinia pretium. Vivamus ac leo pretium faucibus.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-            }
-        </div >
-
-    )
-}
+          <div>
+            <div className="chapterHeader">
+              <h1 id="contacts">Kontakty</h1>
+            </div>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam
+                neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget
+                erat. Aenean fermentum risus id tortor. Fusce consectetuer risus
+                a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus
+                quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non,
+                nonummy ac, erat. Nam sed tellus id magna elementum tincidunt.
+                Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna,
+                cursus sit amet, suscipit a, interdum id, felis. Duis viverra
+                diam non justo. Praesent vitae arcu tempor neque lacinia
+                pretium. Vivamus ac leo pretium faucibus.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam
+                neque. Maecenas aliquet accumsan leo. Etiam bibendum elit eget
+                erat. Aenean fermentum risus id tortor. Fusce consectetuer risus
+                a nunc. Etiam dictum tincidunt diam. Integer imperdiet lectus
+                quis justo. Vestibulum erat nulla, ullamcorper nec, rutrum non,
+                nonummy ac, erat. Nam sed tellus id magna elementum tincidunt.
+                Aliquam ante. Nulla pulvinar eleifend sem. Nulla turpis magna,
+                cursus sit amet, suscipit a, interdum id, felis. Duis viverra
+                diam non justo. Praesent vitae arcu tempor neque lacinia
+                pretium. Vivamus ac leo pretium faucibus.
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Content;
