f045f362b16fd40704907be22fdc765495ab7ed3
Jan Kárník <karnikjan12@gmail.com>
2020-06-07 14:35:39 +0200

    init deploy

---
+++ backend/public/build/precache-manifest.21be113dbb565f14c2b2e072091ec5d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f41116b42bf209bd21e7b546fc836a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0e31c9d5d5013b64375",
+    "url": "/static/css/main.f718eb23.chunk.css"
+  },
+  {
+    "revision": "4553d04d299fc4015a26",
+    "url": "/static/js/2.77622128.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.77622128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e31c9d5d5013b64375",
+    "url": "/static/js/main.f108845a.chunk.js"
+  },
+  {
+    "revision": "c71849e6b92a54d9b65d",
+    "url": "/static/js/runtime-main.dbac8e30.js"
+  },
+  {
+    "revision": "f450f5806ef41e34349aa7919ae12996",
+    "url": "/static/media/hromosvody_header_small.f450f580.png"
+  }
+]);
---
+++ backend/public/build/precache-manifest.2e5e5caeaf3d846e901b76eddcb6e121.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b513fd7eea7235edbeff53aff4e10ae1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0e31c9d5d5013b64375",
+    "url": "./static/css/main.f718eb23.chunk.css"
+  },
+  {
+    "revision": "4553d04d299fc4015a26",
+    "url": "./static/js/2.77622128.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.77622128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e31c9d5d5013b64375",
+    "url": "./static/js/main.f108845a.chunk.js"
+  },
+  {
+    "revision": "57788e0b27679dbd5604",
+    "url": "./static/js/runtime-main.2849aac2.js"
+  },
+  {
+    "revision": "f450f5806ef41e34349aa7919ae12996",
+    "url": "./static/media/hromosvody_header_small.f450f580.png"
+  }
+]);
